9096e79f7f613c0185fab8bfde34fc4f575f2e54
sharlisbg <sharislizza15@gmail.com>
2023-02-10 08:48:30 -0600

    Factoring

--- FactoringVet.java
+++ FactoringVet.java
@@ -1,6 +1,14 @@
 public class RefugioVet{
 
-    public RefugioVet() {
+    public Mascota getMascota(String nombre, String raza, int edad) {
+        Mascota mymascota;
+        if (nombre.equals("Pancho") && raza.equals("Poodle")){
+            mymascota = new Perro(nombre, raza, edad);
+        }
+        if (nombre.equals("Mish") && raza.equals("Persa")){
+            mymascota = new Gato(nombre, raza, edad);
+        }
     }
+
 
 }
